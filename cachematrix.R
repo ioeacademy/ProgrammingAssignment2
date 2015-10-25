8e00241a58795b6309fe7ccc4d14b67d20772c86
Giuseppe Cinque <g.cinque@gmail.com>
2015-10-25 15:27:03 -0700

    Final Version for the R course
    Added comments to the function explaining how it works.

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Submisstion for the Coursera R Programming Coures
 
-## Write a short comment describing this function
+## makeCacheMatric is a fucntion that create a new object that has the property of storing a matrix and its inverse when calculated.
 
 makeCacheMatrix <- function(x = matrix()) {
-    im <- NULL  #inverse matrix null
+    im <- NULL  # Set initially the inverse matrix to null
     set <- function(y) {
-        x <<- y  # add the value of the matrix
-        im <<- NULL #Set the inverse ad nuul
-    }
-    get <- function() x
-    setinverse <- function(inverse) m <<- inverse
-    getinverse <- function() im
-    list(set = set, get = get,
+        x <<- y  # if the method "set"  is invocated the Matrix is stored in the X
+        im <<- NULL # The Inverse matrix value is reset to NULL
+        
+        get <- function() x  # Get Method just returns the Matrix
+        setinverse <- function(inverse) im <<- inverse  # setinverse can be used to write the inverse matrix
+    getinverse <- function() im  # getinverse returns the inverse matrix cached.
+    list(set = set, get = get,   # Returns the new object created
     setinverse = setinverse,
     getinverse = getinverse)
 
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that operate on the new Matrix object, calculate the inverse and store it in the cache variable "im"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        im <- x$getinverse()
-        if(!is.null(im)) {
-            message("getting cached data")
+        im <- x$getinverse()  # Call the method to get the inverse matrix cached
+        if(!is.null(im)) {  # If the cached version is not NULL the fuction returns the cached version and ..
+            message("getting cached data") # dispaly a message allerting that is a cached data.
             return(im)
         }
-        data <- x$get()
-        im <- solve(data, ...)
-        x$setinverse(im)
+        data <- x$get()    # If instead the there is no cached version it takes the data from the matrix...
+        im <- solve(data, ...)  # Calculate the inverse matrix and...
+        x$setinverse(im)  # Stores it in the cache variable
         im
 }
